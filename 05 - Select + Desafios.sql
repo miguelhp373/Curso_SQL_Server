abec38c58f2775c341b59dddd1cf68edf1d26675
Miguel Henrique <miguelhp373@gmail.com>
2021-04-14 11:41:47 -0300

    Create 05 - Select + Desafios.sql

---
+++ 05 - Select + Desafios.sql
@@ -0,0 +1,12 @@
+USE AdventureWorks2017
+
+/*Retorna todos os dados da table Person.Person*/
+SELECT * 
+FROM Person.Person
+
+
+/*A equipe de marketing precisa fazer uma pesquisa sobre os nomes mais comuns de seus clientes e precisa do nome e sobrenome
+de todos os clientes cadastrados no sistema
+*/
+SELECT FirstName, LastName
+FROM Person.Person
