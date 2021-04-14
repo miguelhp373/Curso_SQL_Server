37d321f388bfb0ac43276ab6eda0d5935cf74d85
Miguel Henrique <miguelhp373@gmail.com>
2021-04-14 11:46:06 -0300

    Create 06 - Distinct + Desafios.sql

---
+++ 06 - Distinct + Desafios.sql
@@ -0,0 +1,9 @@
+/*Distinct serve para ocultar dados duplicados de uma table*/
+
+SELECT DISTINCT FirstName 
+FROM Person.Person
+
+/*Quantos nomes unicos temos na table person.person?*/
+
+SELECT LastName
+FROM Person.Person
