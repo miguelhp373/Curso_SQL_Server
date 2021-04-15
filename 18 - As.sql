c699a7e6775ab8df55c422de5bfd8994f6369924
Miguel Henrique <miguelhp373@gmail.com>
2021-04-15 10:58:30 -0300

    Update 18 - As.sql

--- 18 - As.sql
+++ 18 - As.sql
@@ -3,3 +3,25 @@
 
 SELECT TOP 10 ListPrice AS 'PREÇO'
 FROM Production.Product
+
+SELECT TOP 10 AVG(ListPrice) AS 'MÉDIA'
+FROM Production.Product
+
+
+
+-- DESAFIOS
+
+-- 1
+
+SELECT FirstName AS 'PRIMEIRO NOME', LastName AS 'ULTIMO NOME'
+FROM Person.Person
+
+-- 2
+
+SELECT PRODUCTNUMBER AS 'NUMERO DO PRODUTO'
+FROM Production.Product
+
+
+-- 3
+ SELECT UnitPrice AS 'PREÇO UNITÁRIO'
+ FROM Sales.SalesOrderDetail
